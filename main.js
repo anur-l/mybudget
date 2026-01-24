d8625ed02ce89f4d9b46697706de0f7b49146f18
aj <amjamal4050@gmail>
2026-01-24 23:04:43 +0300

    Adding varible to store data

--- main.js
+++ main.js
@@ -1,16 +1,68 @@
+/** @type {HTMLElement | null} */
 const head_sec = document.querySelector(".heading_select");
+/** @type {HTMLElement | null} */
 const combo = document.querySelector(".combo");
-const opt = document.querySelectorAll(".option");
+const option = document.querySelectorAll(".option");
+/** @type {HTMLElement | null} */
+const descrip = document.querySelector("#descp");
+/** @type {HTMLElement | null} */
+const num = document.querySelector("#num");
+/** @type {NodeListOf<HTMLInputElement>} */
+const radios = document.querySelectorAll('input[name="category"]');
 
+let typebudget = {
+  rent: 0,
+  food: 0,
+  transport: 0,
+  utilitie: 0,
+  other: 0,
+};
+
+//const savepersonal = localStorage.getItem("pbudget");
+const savebudget = localStorage.getItem("tbudget");
+if (savebudget) {
+  typebudget = JSON.parse(savebudget);
+}
+
+function savebudger() {
+  if (!num) {
+    alert("Missing input");
+    return;
+  }
+  let selectitem = "";
+  radios.forEach((rb) => {
+    if (rb.checked) selectitem = rb.id;
+  });
+  if (selectitem === "rent") {
+    typebudget.rent += Number(num.value);
+  } else if (selectitem === "food") {
+    typebudget.food += Number(num.value);
+  } else if (selectitem === "utilitie") {
+    typebudget.utilitie += Number(num.value);
+  } else if (selectitem === "transport") {
+    typebudget.transport += Number(num.value);
+  } else {
+    typebudget.other += Number(num.value);
+  }
+  localStorage.setItem("tbudget", JSON.stringify(typebudget));
+  let infobudget = {
+    description: descrip.value,
+    money: Number(num.value),
+    expense: selectitem,
+    date: Date.now(),
+  };
+  descrip.value = "";
+  num.value = "";
+  return infobudget;
+}
 head_sec.addEventListener("click", () => {
-  const displaycss = combo.style.display === "block";
-  combo.style.display = displaycss ? "none" : "block";
+  combo.style.display = combo.style.display === "block" ? "none" : "block";
 });
 
-opt.forEach((opt) => {
+option.forEach((opt) => {
   opt.addEventListener("click", () => {
-    const displaying = opt.querySelector("label").innerText;
-    heading.innerHTML = `Type: <strong>${displaying}</strong>`;
+    let displaying = opt.querySelector("label").innerText;
+    head_sec.innerHTML = `Type: <strong>${displaying}</strong>`;
     combo.style.display = "none";
   });
 });
