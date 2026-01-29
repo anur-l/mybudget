ab082e2fa8186431d77c85fe39e7ca0c9fc0a7f6
aj <amjamal4050@gmail>
2026-01-29 23:44:13 +0300

    Convert html into html5 file format

--- pass.js
+++ pass.js
@@ -1,4 +1,35 @@
-const pin = document.querySelector('#pin');
-const mail = document.querySelector('#mail');
-const password = document.querySelector('#password');
+document.addEventListener("DOMContentLoaded", function() {
+  const sumbtn = document.querySelector(".submit");
 
+  sumbtn.addEventListener("click", function() {
+    const user = document.querySelector("#user");
+    const mail = document.querySelector("#mail");
+    const password = document.querySelector("#password");
+    if (user.value.length < 3) {
+      alert("It must contain atleast 3 character");
+      return;
+    }
+
+    if (!mail.value) {
+      alert("Enter yor email?");
+      return;
+    }
+    if (!mail.value.includes("@")) {
+      alert("Email mist include @");
+      return;
+    }
+
+    if (!mail.value.includes(".")) {
+      alert("Email msut include .");
+      return;
+    }
+
+    if (password.value.length < 6) {
+      alert("Password must be at least 6 characters");
+      return;
+    }
+    user.value = "";
+    password.value = "";
+    mail.value = "";
+  });
+});
